b7165a3f3abf21a91a843ce9a6a7b0e7fb8aad1f
Fureigh <info@fureigh.com>
2014-04-07 15:52:19 -0700

    68918932 - Added index page, title of application and accompanying test.

---
+++ functional_tests.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_load_page_and_see_title(self):
+        # Paramedic wants to report a rider to the MSC. They open this app.
+        self.browser.get('http://localhost:5000')
+
+        # Paramedic sees that the page title mentions 'Notify the MSC'.
+        self.assertIn('Notify the MSC', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
